cfd7445ee0cba8e4b662ffca3e9a10de587094a6
lyh458 <liyihui.ing@gmail.com>
2021-06-17 10:33:49 +0800

    remove baxter camera and fix a ns error

--- scripts/recorder.py
+++ scripts/recorder.py
@@ -12,7 +12,6 @@ from sensor_msgs.msg import Image
 from threading import RLock
 from copy import deepcopy
 from cv_bridge.core import CvBridge
-from baxter_interface.camera import CameraController
 # from thr_infrastructure_msgs.msg import ActionHistoryEvent
 
 
@@ -34,57 +33,33 @@ class Recorder:
 
         # Enabled cameras
         self.cameras_enabled = {'kinect': rospy.get_param('/recorder/enabled/kinect', False),
-                                'depth': rospy.get_param('/recorder/enabled/depth', False),
-                                'left': rospy.get_param('/recorder/enabled/left', True),
-                                'right': rospy.get_param('/recorder/enabled/right', True),
-                                'head': rospy.get_param('/recorder/enabled/head', False)}
-
+                                'depth': rospy.get_param('/recorder/enabled/depth', False)}
+                                
         # Non-cameras sources to record as well
         self.components_enabled = {'frames': rospy.get_param('/recorder/enabled/frames', True),
                                    'actions': rospy.get_param('/recorder/enabled/actions', False)}
 
         # Recording is triggered when all components are ready
         self.readiness_lock = RLock()
-        self.components_ready = {'kinect': False, 'depth': False, 'left': False, 'right': False, 'head': False,
-                                 'clock': not rospy.get_param('use_sim_time', default=False)}
+        self.components_ready = {'kinect': False, 'depth': False, 'clock': not rospy.get_param('use_sim_time', default=False)}
 
         self.frames = rospy.get_param('/recorder/frames') if self.components_enabled['frames'] else []
-        self.image = {'left': None, 'right': None, 'head': None, 'kinect': None, 'depth': None}
-        self.locks = {'left': RLock(), 'right': RLock(), 'head': RLock(),  'kinect': RLock(), 'depth': RLock()}
+        self.image = {'kinect': None, 'depth': None}
+        self.locks = {'kinect': RLock(), 'depth': RLock()}
         # self.four_cc = cv.CV_FOURCC('F' ,'M','P', '4')
         self.four_cc = cv2.VideoWriter_fourcc('F' ,'M','P', '4')
         self.extension = '.avi'
-        self.writers = {'left': None, 'right': None, 'head': None, 'kinect': None, 'depth': None}
-        self.formats = {'left': 'bgr8', 'right': 'bgr8', 'head': 'bgr8', 'kinect': 'bgr8', 'depth': '8UC1'} #'depth': 32FC1?}
+        self.writers = {'kinect': None, 'depth': None}
+        self.formats = {'kinect': 'bgr8', 'depth': '8UC1'} #'depth': 32FC1?}
 
         self.clock_sub = rospy.Subscriber('/clock', Clock, self.cb_clock, queue_size=1)
-        self.right_image_sub = rospy.Subscriber('/cameras/right_hand_camera/image', Image, self.cb_image_right, queue_size=1)
-        self.left_image_sub = rospy.Subscriber('/cameras/left_hand_camera/image', Image, self.cb_image_left, queue_size=1)
-        self.head_image_sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.cb_image_head, queue_size=1)
-        self.kinect_name = rospy.get_param('kinect_name', 'kinectv2')
+        self.kinect_name = rospy.get_param('/recorder/kinect_name', 'kinectv2')
         self.quality = rospy.get_param('quality', 'qhd')
         self.kinect_rgb_topic = [self.kinect_name, self.quality, 'image_color']
         self.kinect_depth_topic = [self.kinect_name, self.quality, 'image_depth_rect']
         self.kinect_rgb_sub = rospy.Subscriber('/'.join(self.kinect_rgb_topic), Image, self.cb_kinect_rgb, queue_size=1)
         self.kinect_depth_sub = rospy.Subscriber('/'.join(self.kinect_depth_topic), Image, self.cb_kinect_depth, queue_size=1)
         # self.actions_sub = rospy.Subscriber('/thr/action_history', ActionHistoryEvent, self.cb_action_history, queue_size=100)
-
-    def start_cameras(self, camera1='left_hand_camera', camera2='right_hand_camera', resolution=(1280, 800)):
-        """
-        Start Baxter cameras. Baxter accepts 2 and only 2 opened cameras due to USB bus restrictions.
-        https://github.com/RethinkRobotics/sdk-docs/wiki/Using-the-Cameras
-        :param camera1: camera name 1
-        :param camera2: camera name 2
-        :param resolution: tuple among 1280x800, 960x600, 640x400, 480x300, 384x240, 320x200
-        """
-        for camera in [camera1, camera2]:
-            short_name = camera.split('_')[0]
-            if self.cameras_enabled[short_name]:
-                rospy.loginfo("Opening camera {} in {}x{} pixels...".format(camera, resolution[0], resolution[1]))
-                controller = CameraController(camera)
-                controller.resolution = resolution
-                controller.open()
-        rospy.loginfo("Cameras opened!")
 
     def get_unready_components(self):
         return [component for component in self.cameras_enabled if self.cameras_enabled[component] and not self.components_ready[component]]
@@ -110,24 +85,6 @@ class Recorder:
             self.image['depth'] = image
         self.open_writer('depth')
         self._update_readiness('depth')
-
-    def cb_image_right(self, image):
-        with self.locks['right']:
-            self.image['right'] = image
-        self.open_writer('right')
-        self._update_readiness('right')
-
-    def cb_image_left(self, image):
-        with self.locks['left']:
-            self.image['left'] = image
-        self.open_writer('left')
-        self._update_readiness('left')
-
-    def cb_image_head(self, image):
-        with self.locks['head']:
-            self.image['head'] = image
-        self.open_writer('head')
-        self._update_readiness('head')
 
     def cb_action_history(self, action):
         events = ['start', 'success', 'failure']
@@ -203,7 +160,6 @@ class Recorder:
             counter = (counter + 1) % 1000
 
     def run(self):
-        self.start_cameras('left_hand_camera', 'right_hand_camera', (1280, 800))
         self.wait_subscribers()
         if not rospy.is_shutdown():
             raw_input("Subscribers ready, press <Enter> to start recording...")
